172784f434a7a018e49c6eda4e0994f167afa961
Joseorina <orinangare@gmail.com>
2019-10-17 04:32:23 +0300

    flask

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.integer(),primary_key=True)
+	username = db.Column(db.String(255))
+	password = db.Column(db.string(255))
+
+	def __init__(self,username):
+    		self.username = username
+
+	def __repr__(self):
+    	 return f"<User '{self.username}>"		
